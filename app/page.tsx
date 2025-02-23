4987e04aabd41a1222c81ad0bf276a4d56639373
boombahtime <boombahtime@gmail.com>
2025-02-23 00:54:07 -0600

    moved components

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,7 @@
 "use client";
 import * as React from "react";
 import Image from "next/image";
-import useSWR from "swr";
-
-const Images = () => {
-  const { data } = useSWR<{ Key?: string }[]>(
-    "/api/documents",
-    async (path: string) => {
-      const f = await fetch(path).then((res) => res.json());
-      return f;
-    }
-  );
-  return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-      {data
-        ?.filter(
-          (image) =>
-            typeof image.Key == "string" &&
-            image.Key != "" &&
-            !/\//.test(image.Key)
-        )
-        .map((image, imageIdx) => (
-          <S3Image Key={image?.Key ? image.Key : "p" + imageIdx} />
-        ))}
-    </div>
-  );
-};
-
-const S3Image = ({ Key }: { Key: string }) => {
-  const [dd, setDD] = React.useState<string | null>(null);
-  React.useEffect(() => {
-    async function bo() {
-      const da = await fetch(`/api/documents/${Key}`);
-      const data = await da.json();
-      setDD(data.src);
-    }
-    bo();
-  }, [Key]);
-  return dd ? (
-    <div className="h-auto max-w-full rounded-lg">
-      <Image width={100} height={100} key={Key} alt="asshoole" src={dd} />
-    </div>
-  ) : null;
-};
+import { Images } from "./components";
 
 export default function Home() {
   const inputRef = React.useRef<HTMLInputElement>();
