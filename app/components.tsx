7b285be46a91cb74c55422797286cb198523528d
boombahtime <boombahtime@gmail.com>
2025-02-23 17:41:18 -0600

    fixed auto reload of images

--- app/components.tsx
+++ app/components.tsx
@@ -2,18 +2,48 @@ import * as React from "react";
 import useSWR from "swr";
 import Image from "next/image";
 
+export const LoadingSvg = ({ loading }: { loading: boolean }) => {
+  return (
+    loading && (
+      <svg
+        aria-hidden="true"
+        role="status"
+        className="inline w-4 h-4 me-3 text-white animate-spin"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="#E5E7EB"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentColor"
+        />
+      </svg>
+    )
+  );
+};
+
 export const Images = ({ loading }: { loading: boolean }) => {
   console.log("app/components/Images");
 
-  const { data } = useSWR<{ Key?: string }[]>(
-    "/api/documents",
-    async (path: string) => {
-      console.log("fetch(", path, ")");
-      const f = await fetch(path).then((res) => res.json());
+  const [data, setData] = React.useState<{ Key: string }[] | null>(null);
+
+  React.useEffect(() => {
+    async function bop() {
+      console.log("fetch(/api/documents)");
+      const f = await fetch("/api/documents").then((res) => res.json());
       console.log("f:", f);
-      return f;
+      setData(f);
+      // return f;
     }
-  );
+    bop();
+    console.log("asdf");
+  }, [loading]);
+
+  // const { data } = useSWR<{ Key?: string }[]>();
   const audios = data?.filter(
     (image) =>
       typeof image.Key == "string" &&
@@ -31,8 +61,8 @@ export const Images = ({ loading }: { loading: boolean }) => {
   return (
     <>
       {images && (
-        <>
-          <h2 className="w-full text-center text-2xl font-bold p-4">Images</h2>
+        <div className="pb-2">
+          <h2 className="w-full text-center text-2xl font-bold pb-4">Images</h2>
           <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
             {images.map((image, imageIdx) => (
               <S3Image
@@ -41,21 +71,22 @@ export const Images = ({ loading }: { loading: boolean }) => {
               />
             ))}
           </div>
-        </>
+        </div>
       )}
       {audios && (
-        <>
-          <h2 className="w-full text-center text-2xl font-bold p-4">Audios</h2>
-          <div id="audios" className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="gap-2">
+          <h2 className="w-full text-center flex text-2xl font-bold p-4">
+            Audios
+          </h2>
+          <div id="audios" className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {audios.map((image, imageIdx) => (
-              <figure key={image.Key}>
-                <figcaption>Listen to {image.Key?.slice(10, 20)}:</figcaption>
-                <audio controls src={image.Key}></audio>
-                <a href={image.Key}> Download audio </a>
-              </figure>
+              <S3Audio
+                key={image.Key}
+                Key={image?.Key ? image.Key : "p" + imageIdx}
+              />
             ))}
           </div>
-        </>
+        </div>
       )}
     </>
   );
@@ -74,7 +105,7 @@ const S3Image = ({ Key }: { Key: string }) => {
   return dd ? (
     <div
       key={Key}
-      className="h-auto max-w-full rounded-lg bg-slate-100 justify-center items-center flex p-4"
+      className="h-auto max-w-full rounded-xl bg-slate-200 justify-center items-center flex p-4"
     >
       <Image
         placeholder="blur"
@@ -85,6 +116,30 @@ const S3Image = ({ Key }: { Key: string }) => {
         alt="asshoole"
         src={dd}
       />
+    </div>
+  ) : null;
+};
+
+const S3Audio = ({ Key }: { Key: string }) => {
+  const [dd, setDD] = React.useState<string | null>(null);
+  React.useEffect(() => {
+    async function bo() {
+      const da = await fetch(`/api/documents/${Key}`);
+      const data = await da.json();
+      setDD(data.src);
+    }
+    bo();
+  }, [Key]);
+  return dd ? (
+    <div
+      key={Key}
+      className="h-auto max-w-full rounded-lg bg-slate-100 justify-center items-center flex p-4"
+    >
+      <figure key={Key}>
+        <figcaption>Listen to {Key?.slice(10, 20)}:</figcaption>
+        <audio src={dd} controls></audio>
+        <a href={Key}> Download audio </a>
+      </figure>
     </div>
   ) : null;
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
 "use client";
 import * as React from "react";
 import Image from "next/image";
-import { Images } from "./components";
+import { Images, LoadingSvg } from "./components";
 
 export default function Home() {
   const inputRef = React.useRef<HTMLInputElement>(null);
   const [uploading, setUploading] = React.useState(false);
+
+  async function handleUploadClick() {
+    setUploading(true);
+    try {
+      const input: HTMLInputElement | null = inputRef.current;
+      if (!input) {
+        return;
+      }
+      const file: File | null =
+        input?.files && input.files.length > 0 ? input.files[0] : null;
+      if (!file) {
+        return;
+      }
+      const body = new FormData();
+      body.append("file", file, file.name);
+      const response = await fetch("/api/documents", {
+        method: "POST",
+        body,
+      });
+      await response.json();
+      if (inputRef?.current) {
+        inputRef.current.value = "";
+      }
+    } catch (e) {
+      console.error(e);
+      setUploading(false);
+    }
+    setUploading(false);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 gap-8">
       {/* 
@@ -16,7 +46,7 @@ export default function Home() {
 ██████╔╝╚██████╔╝╚██████╔╝██║ ╚═╝ ██║██████╔╝██║  ██║██║  ██║
 ╚═════╝  ╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
                                                               */}
-      <div>
+      <div className="h-[150px]">
         <div className="relative top-[-50px] flex place-items-center before:absolute before:h-[300px] before:w-[380px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[160px] after:w-[180px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[260px] z-[-1]">
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -28,63 +58,20 @@ export default function Home() {
           />
         </div>
       </div>
+
       <div>
         <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm flex">
-          <div className="bg-blue-200 p-4 opacity-90">
-            <input className="bg-red-200" type="file" ref={inputRef} />
+          <div className="flex flex-wrap flex-row p-4 opacity-90">
+            <input className="flex bg-slate-500" type="file" ref={inputRef} />
             <button
               disabled={!!uploading}
-              onClick={async () => {
-                setUploading(true);
-                const input: HTMLInputElement | null = inputRef.current;
-                if (!input) {
-                  return;
-                }
-                const file: File | null =
-                  input?.files && input.files.length > 0
-                    ? input.files[0]
-                    : null;
-                if (!file) {
-                  return;
-                }
-                const body = new FormData();
-                body.append("file", file, file.name);
-                const response = await fetch("/api/documents", {
-                  method: "POST",
-                  body,
-                });
-                await response.json();
-                if (inputRef?.current) {
-                  inputRef.current.value = "";
-                }
-                setUploading(false);
-              }}
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+              onClick={handleUploadClick}
+              className="flex w-full relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
             >
-              {uploading ? (
-                <svg
-                  aria-hidden="true"
-                  role="status"
-                  className="inline w-4 h-4 me-3 text-white animate-spin"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="#E5E7EB"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentColor"
-                  />
-                </svg>
-              ) : (
-                <></>
-              )}
+              <LoadingSvg loading={uploading} />
               <span
                 className={
-                  "relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent"
+                  "relative px-5 py-2.5 transition-all ease-in duration-75 bg-whidte dark:bg-grday-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent"
                 }
               >
                 keep it
@@ -105,7 +92,7 @@ export default function Home() {
      \::/  /   \:\  \        \:\  \        \:\/:/  /     \:\/:/  /     \/_/:/  /  
      /:/  /     \:\__\        \:\__\        \::/  /       \::/  /        /:/  /   
      \/__/       \/__/         \/__/         \/__/         \/__/         \/__/     */}
-      <div className="bg-red-100 absoslute b-0 r-0 w-9/10 h-auto rounded-xl p-12">
+      <div className="bg-slate-900 absoslute b-0 r-0 w-full h-auto rounded-xl p-4 sm:p-8 lg:p-12">
         <Images loading={!!uploading} />
       </div>
       {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"> */}
