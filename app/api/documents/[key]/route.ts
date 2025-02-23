da1e614b11e0bd94845111c611299a5dae66de9e
boombahtime <boombahtime@gmail.com>
2025-02-23 16:11:47 -0600

    fixed load image api endpoint

--- app/api/documents/[key]/route.ts
+++ app/api/documents/[key]/route.ts
@@ -4,13 +4,16 @@ import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client } from "@aws-sdk/client-s3";
 
-const Bucket = "boombahtime-media";
-
-const s3 = new S3Client({});
+const BUCKET_NAME = "boombahtime-media";
+const BUCKET_REGION = "us-east-2";
+const s3 = new S3Client({ region: BUCKET_REGION });
 
 // Bucket and s3: same as above
 export async function GET(_: Request, { params }: { params: { key: string } }) {
-  const command = new GetObjectCommand({ Bucket, Key: params.key });
+  const command = new GetObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: params.key,
+  });
   const src = await getSignedUrl(s3, command, { expiresIn: 3600 });
   return NextResponse.json({ src });
 }
